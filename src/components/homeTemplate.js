6c491fbb7c6c64f2d02fb60387cd777922faafb7
Favas Cherukunnu <mhdfavascheru@gmail.com>
2023-05-17 22:34:50 +0530

    completed design by using bootstrap

--- src/components/homeTemplate.js
+++ src/components/homeTemplate.js
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { BiHomeAlt, BiSearch } from 'react-icons/bi';
+import { BiHomeAlt, BiSearch,BiExpand} from 'react-icons/bi';
 import { MdOutlineNotificationsNone } from 'react-icons/md';
 import './navbar.css';
 import { Col, Row, Stack } from "react-bootstrap";
@@ -19,46 +19,60 @@ export default function HomeTemplate() {
     });
 
     return (
-        <div>
-            <div className="outerDiv p-2">
-                <Navbar className="topNavBar" bg="light" expand="lg">
-                    <Container fluid>
-                        {/* <Navbar.Brand href="#">Navbar scroll</Navbar.Brand> */}
-                        <Navbar.Toggle aria-controls="sideNavBarScroll" />
+        <div className="navBarOuter p-2">
+            <Row className=" p-0 m-0" style={{height:'100%'}}>
+                <Col xs={12} md={3} lg={3} xl={2} className="p-0 me-2">
+                    {/* <div className=''> */}
+                    <Stack className='sideNavBar'>
+                        <Nav defaultActiveKey="/home" className="flex-column">
+                            <Nav.Link href="/home">Active</Nav.Link>
+                            <Nav.Link eventKey="link-1">Link</Nav.Link>
+                            <Nav.Link eventKey="link-2">Link</Nav.Link>
+                            <Nav.Link eventKey="disabled" disabled className='mt-auto'>
+                                Disabled
+                            </Nav.Link>
+                        </Nav>
+                    </Stack>
+                    {/* </div> */}
+                    {/* <div style={{backgroundColor:'white',height:'500px'}}>
 
-                        <Stack className="ms-auto" direction="horizontal" gap={3}>
-                            <Form >
-                                <Stack direction="horizontal">
-                                    <Form.Control
-                                        type="search"
-                                        placeholder="Search"
-                                        className="me-2"
-                                        aria-label="Search"
-                                    />
-                                    <BiSearch size={25} />
+                    </div> */}
+                </Col>
+                <Col className="p-0 " >
+                    <div className="outerDiv">
+                        <Navbar className="topNavBar" bg="light" expand="lg">
+                            <Container fluid>
+                                <Navbar.Toggle aria-controls="sideNavBarScroll" />
+
+                                <BiExpand size={25}/>
+
+                                <Stack className="ms-auto" direction="horizontal" gap={3}>
+                                    <Form >
+                                        <Stack direction="horizontal">
+                                            <Form.Control
+                                                type="search"
+                                                placeholder="Search"
+                                                className="me-2"
+                                                aria-label="Search"
+                                            />
+                                            <BiSearch size={25} />
+                                        </Stack>
+                                    </Form>
+                                    <MdOutlineNotificationsNone size={25} />
+                                    <BiHomeAlt size={25} />
                                 </Stack>
-                            </Form>
-                            <MdOutlineNotificationsNone size={25} />
-                            <BiHomeAlt size={25} />
-                        </Stack>
-                    </Container>
-                </Navbar>
-                <div className="contentDiv pt-2">
-                    <div className="contentInnerDiv"></div>
-                </div>
-            </div>
-            <div className='navBarOuter p-2'>
-                <Stack className='sideNavBar'>
-                    <Nav defaultActiveKey="/home" className="flex-column">
-                        <Nav.Link href="/home">Active</Nav.Link>
-                        <Nav.Link eventKey="link-1">Link</Nav.Link>
-                        <Nav.Link eventKey="link-2">Link</Nav.Link>
-                        <Nav.Link eventKey="disabled" disabled className='mt-auto'>
-                            Disabled
-                        </Nav.Link>
-                    </Nav>
-                </Stack>
-            </div>
+                            </Container>
+                        </Navbar>
+                        <div className="contentDiv pt-2">
+                            <div className="contentInnerDiv p-4">njh</div>
+                        </div>
+                    </div>
+
+                    {/* <div style={{backgroundColor:'white',height:'500px'}}>
+
+                    </div> */}
+                </Col>
+            </Row>
         </div>
     );
 
