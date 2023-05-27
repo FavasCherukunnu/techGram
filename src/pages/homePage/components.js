f7dfb77246d08a46cdbd04e48ce66f3cfebb0f51
Favas Cherukunnu <mhdfavascheru@gmail.com>
2023-05-27 20:26:05 +0530

    mouse hover added:

--- src/pages/homePage/components.js
+++ src/pages/homePage/components.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { NavLink, Nav ,Button} from 'react-bootstrap';
 
@@ -16,10 +16,18 @@ export function RectangleButton(props){
 
     //props
     //width - width of button
+    const [isHoverded, setHover] = useState(false);
+    const buttonStyle = {style:{background:isHoverded===false?'#81F14D':'#76db47',borderStyle:"none",boxShadow:"6px 4px 8px -2px rgba(0, 0, 0, 0.08)",minWidth:'100px',"width":props.width,fontFamily:"'Alumni Sans'",fontStyle:"normal",fontWeight:"600",fontSize:"24px",color:"#FFFFFF",textShadow:"6px 4px 8px rgba(0, 0, 0, 0.08)"}}
 
-    const buttonStyle = {"background":"#81F14D","borderStyle":"none","boxShadow":"6px 4px 8px -2px rgba(0, 0, 0, 0.08)","minWidth":'100px',"width":props.width,"fontFamily":"'Alumni Sans'","fontStyle":"normal","fontWeight":"600","fontSize":"24px","color":"#FFFFFF","textShadow":"6px 4px 8px rgba(0, 0, 0, 0.08)"}
+    function mouseOverEvent(){
+        setHover(true);
+    }
 
+    function mouseLeaveEvent(){
+        setHover(false);
+    }
+    
     return (
-        <Button  style={buttonStyle} >{props.children}</Button>
+        <Button  style={buttonStyle.style} onMouseEnter={mouseOverEvent} onMouseLeave={mouseLeaveEvent} >{props.children}</Button>
     );
 }
