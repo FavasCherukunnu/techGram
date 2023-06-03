48ee284ac823b8d8d088e02aa53a66da40b2db64
Favas Cherukunnu <mhdfavascheru@gmail.com>
2023-06-03 20:05:13 +0530

    starting backend

--- src/pages/loginPage/LoginPage.js
+++ src/pages/loginPage/LoginPage.js
@@ -2,34 +2,37 @@ import React, { useState } from 'react';
 import './loginPage.css';
 import { FormInput } from './component';
 import { RectangleButton } from '../../components/buttonRectangle';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    email: 'mhdfavascheru@gmail.com',
+    password: '1234',
   });
 
   const handleOnChange = (event) => {
     const name = event.target['name'];
-    if (name === 'dob.day') {
-      formData['dob']['day'] = event.target.value;
-    } else if (name === 'dob.month') {
-      formData['dob']['month'] = event.target.value;
-    } else if (name === 'dob.year') {
-      formData['dob']['year'] = event.target.value;
-    } else {
-      formData[name] = event.target.value;
-    }
+    formData[name] = event.target.value;
+
     setFormData({ ...formData });
   };
 
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async(event) => {
     event.preventDefault();
-    navigate('/home');
+    console.log(formData);
+    try {
+      await axios.post('http://localhost:3002/api/login', formData).then((res) => {
+        localStorage.setItem('auth-token', res.data.token);
+        navigate('/home')
+      })
+    } catch (err) {
+      console.log(err.response);
+    }
+    // navigate('/home');
     // Perform signup logic here
     // For example, you can make an API request to register the user
 
@@ -39,25 +42,25 @@ const LoginPage = () => {
   return (
     <div className=" signup-page rootDiv">
       <div className='topFlexiv'>
-        <div style={{overflowY:'auto',padding:'20px'}}>
-        <h1 className="hero_title">LOGIN</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="gridDiv">
-            <div className='loginPage_gridItem'>
-              <FormInput inputTitle='Email' onChange={handleOnChange} width='100%' name='email' value={formData.fullName} placeholder="Email" />
-              <FormInput inputTitle='Password' width='100%' onChange={handleOnChange} name='password' value={formData.address} placeholder="Password" />
-              <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', paddingTop: '22px' }}>
-                <div style={{height:'60px',lineHeight:'0.6'}}>
-                    
+        <div style={{ overflowY: 'auto', padding: '20px' }}>
+          <h1 className="hero_title">LOGIN</h1>
+          <form>
+            <div className="gridDiv">
+              <div className='loginPage_gridItem'>
+                <FormInput inputTitle='Email' onChange={handleOnChange} width='100%' name='email' value={formData.email} placeholder="Email" />
+                <FormInput inputTitle='Password' width='100%' onChange={handleOnChange} name='password' value={formData.password} placeholder="Password" />
+                <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', paddingTop: '22px' }}>
+                  <div style={{ height: '60px', lineHeight: '0.6' }}>
+
                     <Link ><p className='linkText'>Forget Password?</p></Link>
                     <Link to={'/signUp'}><p className='linkText'>New User? Sign Up</p></Link>
+                  </div>
+                  <RectangleButton width='150px' onClick={handleSubmit}>LOGIN</RectangleButton>
                 </div>
-                <RectangleButton width='150px' onClick={handleSubmit}>LOGIN</RectangleButton>
               </div>
             </div>
-          </div>
 
-        </form>
+          </form>
         </div>
       </div>
     </div>
--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -3,25 +3,51 @@ import './SignupPage.css';
 import { FormInput } from './component';
 import { RectangleButton } from '../../components/buttonRectangle';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const SignupPage = () => {
+  // const [formData, setFormData] = useState({
+  //   fullName: '',
+  //   address: '',
+  //   phoneNo: '',
+  //   email: '',
+  //   fatherName: '',
+  //   motherName: '',
+  //   district: '',
+  //   taluk: '',
+  //   panchayath: '',
+  //   wardNo: '',
+  //   pinCode: '',
+  //   dob: { day: '', month: '', year: '' },
+  //   adharNo: '',
+  //   dataTimeNow: '',
+  //   image:'',
+  //   isApproved:false
+  // });
+
+  let navigate = useNavigate();
+
   const [formData, setFormData] = useState({
-    fullName: '',
-    address: '',
-    phoneNo: '',
-    email: '',
-    fatherName: '',
-    motherName: '',
-    district: '',
-    taluk: '',
-    panchayath: '',
-    wardNo: '',
-    pinCode: '',
-    dob: { day: '', month: '', year: '' },
-    adharNo: '',
-    dataTimeNow: '',
-    image:''
+    fullName: 'favas mohammed',
+    address: 'pothanooran',
+    phoneNo: '7994020923',
+    email: 'mhdfavascheru@gmail.com',
+    fatherName: 'abdul majeed',
+    motherName: 'maimoona',
+    district: 'malappuram',
+    taluk: 'tirur',
+    panchayath: 'othukkungal',
+    wardNo: '11',
+    pinCode: '676528',
+    dob: { day: '28', month: '05', year: '2000' },
+    adharNo: '9990234566789',
+    password: '1234',
+    dateTimeNow: 'null',
+    image: '',
+    isApproved: false
   });
+
 
   const handleOnChange = (event) => {
     const name = event.target['name'];
@@ -31,6 +57,10 @@ const SignupPage = () => {
       formData['dob']['month'] = event.target.value;
     } else if (name === 'dob.year') {
       formData['dob']['year'] = event.target.value;
+    } else if (name === 'profileImage') {
+      // console.log(event.target.files[0]);
+      formData['image'] = event.target.files[0];
+
     } else {
       formData[name] = event.target.value;
     }
@@ -38,8 +68,22 @@ const SignupPage = () => {
   };
 
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
+    const form = new FormData();
     event.preventDefault();
+    form.append('data1', JSON.stringify(formData))
+    form.append('image', formData.image);
+
+    // formData.dataTimeNow = 
+    try {
+      axios.post('http://localhost:3002/api/register', form, { headers: localStorage.getItem('token') }).then((res) => {
+        localStorage.setItem('auth-token',res.data.token);
+        navigate('/login');
+      })
+    } catch(err) {
+      console.log(err);
+      console.log('Not logged in');
+    }
 
     // Perform signup logic here
     // For example, you can make an API request to register the user
@@ -50,49 +94,51 @@ const SignupPage = () => {
   return (
     <div className=" signup-page rootDiv">
       <div className='topFlexiv'>
-        <div style={{overflowY:'auto',padding:'20px'}}>
-        <h1 className="hero_title">SIGN UP</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="gridDiv">
-            <div className='gridItem'>
-              <FormInput inputTitle='Full Name' onChange={handleOnChange} width='100%' name='fullName' value={formData.fullName} placeholder="Full Name" />
-              <FormInput inputTitle='Address' width='100%' onChange={handleOnChange} name='address' value={formData.address} placeholder="Address" />
-              <FormInput inputTitle='Father Name' width='100%' onChange={handleOnChange} name='fatherName' value={formData.fatherName} placeholder="Father Name" />
-              <FormInput inputTitle='Mother Name' width='100%' onChange={handleOnChange} name='motherName' value={formData.motherName} placeholder="Mother Name" />
-              <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                <FormInput inputTitle='Phone No' width='48%' onChange={handleOnChange} name='phoneNo' value={formData.phoneNo} placeholder="Phone No" />
-                <FormInput inputTitle='Email' width='48%' onChange={handleOnChange} name='email' value={formData.email} placeholder="Email" />
-              </div>
-            </div>
-            <div className='gridItem'>
-              <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                <FormInput inputTitle='District' width='48%' onChange={handleOnChange} name='district' value={formData.district} placeholder="District" />
-                <FormInput inputTitle='Taluk' width='48%' onChange={handleOnChange} name='taluk' value={formData.taluk} placeholder="Taluk" />
-              </div>
-              <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                <FormInput inputTitle='Panchayath' width='48%' onChange={handleOnChange} name='panchayath' value={formData.panchayath} placeholder="Panchayath" />
-                <FormInput inputTitle='Ward No' width='48%' onChange={handleOnChange} name='wardNo' value={formData.wardNo} placeholder="Ward No" />
-              </div>
-              <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                <FormInput inputTitle='Pin Code' width='48%' onChange={handleOnChange} name='pinCode' value={formData.pinCode} placeholder="Pin Code" />
-                <div style={{ width: '48%', display: 'flex', justifyContent: 'space-between' }}>
-                  <FormInput inputTitle='Day' width='30%' onChange={handleOnChange} name='dob.day' value={formData.dob.day} placeholder="Day" />
-                  <FormInput inputTitle='Month' width='30%' onChange={handleOnChange} name='dob.month' value={formData.dob.month} placeholder="Month" />
-                  <FormInput inputTitle='Year' width='30%' onChange={handleOnChange} name='dob.year' value={formData.dob.year} placeholder="Year" />
+        <div style={{ overflowY: 'auto', padding: '20px' }}>
+          <h1 className="hero_title">SIGN UP</h1>
+          <form>
+            <div className="gridDiv">
+              <div className='gridItem'>
+                <FormInput inputTitle='Full Name' onChange={handleOnChange} width='100%' name='fullName' value={formData.fullName} placeholder="Full Name" />
+                <FormInput inputTitle='Address' width='100%' onChange={handleOnChange} name='address' value={formData.address} placeholder="Address" />
+                <FormInput inputTitle='Father Name' width='100%' onChange={handleOnChange} name='fatherName' value={formData.fatherName} placeholder="Father Name" />
+                <FormInput inputTitle='Mother Name' width='100%' onChange={handleOnChange} name='motherName' value={formData.motherName} placeholder="Mother Name" />
+                <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+                  <FormInput inputTitle='Phone No' width='48%' onChange={handleOnChange} name='phoneNo' value={formData.phoneNo} placeholder="Phone No" />
+                  <FormInput inputTitle='Email' width='48%' onChange={handleOnChange} name='email' value={formData.email} placeholder="Email" />
                 </div>
+                <FormInput inputTitle='Profile image' type='file' name='profileImage' onChange={handleOnChange} />
               </div>
-              <FormInput inputTitle='Adhar No' width='100%' onChange={handleOnChange} name='adharNo' value={formData.adharNo} placeholder="Adhar No" />
-              <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', paddingTop: '22px' }}>
-              <div style={{height:'60px',lineHeight:'0.6'}}>
-                    
-                    <Link to={'/login'}><p className='linkText'>Already User? Login</p></Link>
+              <div className='gridItem'>
+                <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+                  <FormInput inputTitle='District' width='48%' onChange={handleOnChange} name='district' value={formData.district} placeholder="District" />
+                  <FormInput inputTitle='Taluk' width='48%' onChange={handleOnChange} name='taluk' value={formData.taluk} placeholder="Taluk" />
                 </div>
-                <RectangleButton width='150px'>SIGNUP</RectangleButton>
-              </div>
-            </div>
-          </div>
+                <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+                  <FormInput inputTitle='Panchayath' width='48%' onChange={handleOnChange} name='panchayath' value={formData.panchayath} placeholder="Panchayath" />
+                  <FormInput inputTitle='Ward No' width='48%' onChange={handleOnChange} name='wardNo' value={formData.wardNo} placeholder="Ward No" />
+                </div>
+                <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+                  <FormInput inputTitle='Pin Code' width='48%' onChange={handleOnChange} name='pinCode' value={formData.pinCode} placeholder="Pin Code" />
+                  <div style={{ width: '48%', display: 'flex', justifyContent: 'space-between' }}>
+                    <FormInput inputTitle='Day' width='30%' onChange={handleOnChange} name='dob.day' value={formData.dob.day} placeholder="Day" />
+                    <FormInput inputTitle='Month' width='30%' onChange={handleOnChange} name='dob.month' value={formData.dob.month} placeholder="Month" />
+                    <FormInput inputTitle='Year' width='30%' onChange={handleOnChange} name='dob.year' value={formData.dob.year} placeholder="Year" />
+                  </div>
+                </div>
+                <FormInput inputTitle='Adhar No' width='100%' onChange={handleOnChange} name='adharNo' value={formData.adharNo} placeholder="Adhar No" />
+                <FormInput inputTitle='Password' width='100%' onChange={handleOnChange} name='password' value={formData.password} placeholder="Password" />
+                <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', paddingTop: '22px' }}>
+                  <div style={{ height: '60px', lineHeight: '0.6' }}>
 
-        </form>
+                    <Link to={'/login'}><p className='linkText'>Already User? Login</p></Link>
+                  </div>
+                  <RectangleButton width='150px' onClick={handleSubmit}>SIGNUP</RectangleButton>
+                </div>
+              </div>
+            </div>
+
+          </form>
         </div>
       </div>
     </div>
