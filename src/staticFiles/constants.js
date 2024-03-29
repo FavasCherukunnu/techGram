59e8f95ba860446b4b0cb6b6ce678c0eca7edd22
unknown <mhdfavascheru@gmail.com>
2024-03-29 23:27:34 +0530

    updated environment variable

--- src/staticFiles/constants.js
+++ src/staticFiles/constants.js
@@ -1,2 +1,2 @@
-export const SERVER_ADDRESS = 'http://localhost:3002';
+export const SERVER_ADDRESS = process.env.REACT_APP_BACKEND_URL;
 // export const SERVER_ADDRESS = 'http://192.168.46.1:3002';
